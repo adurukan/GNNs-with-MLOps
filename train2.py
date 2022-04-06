3d7fc80b279110d968577f94c75077cf44561737
Alpay Sedat Durukan <alpay.sedat.durukan@pwc.com>
2022-04-06 11:38:14 +0000

    checking whether imports are correct

---
+++ train2.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torch_geometric.data import Data
+from torch_geometric.nn import GATConv
+from torch_geometric.datasets import Planetoid
+from torch_geometric.utils import from_networkx
+import torch_geometric.transforms as T
+
+import matplotlib.pyplot as plt
+
+import networkx as nx
+from networkx.generators.random_graphs import erdos_renyi_graph
+import matplotlib.pyplot as plt
+from random import randint
+
+print("Imports are all correct")
